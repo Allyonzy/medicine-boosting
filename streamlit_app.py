179c854f456065606e21fed02f747280103bf52c
Allyonzy <64588181+Allyonzy@users.noreply.github.com>
2024-10-18 17:48:02 +0000

    Добавлены страницы для приложения

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,66 +1,100 @@
-import altair as alt
-import pandas as pd
+# import altair as alt
+# import pandas as pd
+# import streamlit as st
+
+# # Show the page title and description.
+# st.set_page_config(page_title="Movies dataset", page_icon="🎬")
+# st.title("🎬 Movies dataset")
+# st.write(
+#     """
+#     This app visualizes data from [The Movie Database (TMDB)](https://www.kaggle.com/datasets/tmdb/tmdb-movie-metadata).
+#     It shows which movie genre performed best at the box office over the years. Just 
+#     click on the widgets below to explore!
+#     """
+# )
+
+
+# # Load the data from a CSV. We're caching this so it doesn't reload every time the app
+# # reruns (e.g. if the user interacts with the widgets).
+# @st.cache_data
+# def load_data():
+#     df = pd.read_csv("data/movies_genres_summary.csv")
+#     return df
+
+
+# df = load_data()
+
+# # Show a multiselect widget with the genres using `st.multiselect`.
+# genres = st.multiselect(
+#     "Genres",
+#     df.genre.unique(),
+#     ["Action", "Adventure", "Biography", "Comedy", "Drama", "Horror"],
+# )
+
+# # Show a slider widget with the years using `st.slider`.
+# years = st.slider("Years", 1986, 2006, (2000, 2016))
+
+# # Filter the dataframe based on the widget input and reshape it.
+# df_filtered = df[(df["genre"].isin(genres)) & (df["year"].between(years[0], years[1]))]
+# df_reshaped = df_filtered.pivot_table(
+#     index="year", columns="genre", values="gross", aggfunc="sum", fill_value=0
+# )
+# df_reshaped = df_reshaped.sort_values(by="year", ascending=False)
+
+
+# # Display the data as a table using `st.dataframe`.
+# st.dataframe(
+#     df_reshaped,
+#     use_container_width=True,
+#     column_config={"year": st.column_config.TextColumn("Year")},
+# )
+
+# # Display the data as an Altair chart using `st.altair_chart`.
+# df_chart = pd.melt(
+#     df_reshaped.reset_index(), id_vars="year", var_name="genre", value_name="gross"
+# )
+# chart = (
+#     alt.Chart(df_chart)
+#     .mark_line()
+#     .encode(
+#         x=alt.X("year:N", title="Year"),
+#         y=alt.Y("gross:Q", title="Gross earnings ($)"),
+#         color="genre:N",
+#     )
+#     .properties(height=320)
+# )
+# st.altair_chart(chart, use_container_width=True)
+
+
 import streamlit as st
-
-# Show the page title and description.
-st.set_page_config(page_title="Movies dataset", page_icon="🎬")
-st.title("🎬 Movies dataset")
-st.write(
-    """
-    This app visualizes data from [The Movie Database (TMDB)](https://www.kaggle.com/datasets/tmdb/tmdb-movie-metadata).
-    It shows which movie genre performed best at the box office over the years. Just 
-    click on the widgets below to explore!
-    """
-)
+from streamlit.logger import get_logger
 
 
-# Load the data from a CSV. We're caching this so it doesn't reload every time the app
-# reruns (e.g. if the user interacts with the widgets).
-@st.cache_data
-def load_data():
-    df = pd.read_csv("data/movies_genres_summary.csv")
-    return df
+LOGGER = get_logger(__name__) # регистрация дейсвтвий на ресурсе
 
-
-df = load_data()
-
-# Show a multiselect widget with the genres using `st.multiselect`.
-genres = st.multiselect(
-    "Genres",
-    df.genre.unique(),
-    ["Action", "Adventure", "Biography", "Comedy", "Drama", "Horror"],
-)
-
-# Show a slider widget with the years using `st.slider`.
-years = st.slider("Years", 1986, 2006, (2000, 2016))
-
-# Filter the dataframe based on the widget input and reshape it.
-df_filtered = df[(df["genre"].isin(genres)) & (df["year"].between(years[0], years[1]))]
-df_reshaped = df_filtered.pivot_table(
-    index="year", columns="genre", values="gross", aggfunc="sum", fill_value=0
-)
-df_reshaped = df_reshaped.sort_values(by="year", ascending=False)
-
-
-# Display the data as a table using `st.dataframe`.
-st.dataframe(
-    df_reshaped,
-    use_container_width=True,
-    column_config={"year": st.column_config.TextColumn("Year")},
-)
-
-# Display the data as an Altair chart using `st.altair_chart`.
-df_chart = pd.melt(
-    df_reshaped.reset_index(), id_vars="year", var_name="genre", value_name="gross"
-)
-chart = (
-    alt.Chart(df_chart)
-    .mark_line()
-    .encode(
-        x=alt.X("year:N", title="Year"),
-        y=alt.Y("gross:Q", title="Gross earnings ($)"),
-        color="genre:N",
+def run():
+    st.set_page_config(
+        page_title="Классификатор",
     )
-    .properties(height=320)
-)
-st.altair_chart(chart, use_container_width=True)
+
+    st.write("# Задача машинного обучения")
+
+    st.sidebar.success("Выберите раздел")
+
+    st.markdown(
+        """
+        Использование машинного обучения для классификации по признаку эндокринного заболевания
+        **👈 Список разделов** to see some examples
+        of what Streamlit can do!
+        ### Want to learn more?
+        - EDA [Анализ датасета и его описание](./EDA)
+        - Демонстрация датафрейма [здесь](./DataFrame_Demo)
+        - Тестирование модели [здесь](./Work_with_model)
+
+        ### See more complex demos
+    """
+    )
+
+
+if __name__ == "__main__":
+    run()
